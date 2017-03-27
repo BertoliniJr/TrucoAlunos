37fe18f80b6f5a3acd391511f66f4917bbea78c7
aluno <aluno@BRPLI0424>
2017-03-27 14:32:21 -0300

    Jogador Ilusionista

---
+++ Truco/IlusionistaDaMesa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CardGame;
+
+namespace Truco
+{
+    class IlusionistaDaMesa : Jogador
+    {
+        public IlusionistaDaMesa(string n) : base(n)
+        {
+        }
+
+        public override Carta Jogar(List<Carta> cartasRodada, Carta manilha)
+        {
+            return null;
+        }
+        public void Correr()
+        {
+
+        }
+    }
+}
