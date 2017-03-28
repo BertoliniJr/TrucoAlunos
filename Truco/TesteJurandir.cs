6aca42c70b97bfffce082f47a27e31c7b23b2057
aluno <aluno@BRPLI0432>
2017-03-28 10:36:31 -0300

    teste jurandir

---
+++ Truco/TesteJurandir.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame
+{
+    class TesteJurandir
+    {
+
+        public static int Teste1(Jogador Oponente)
+        {
+            int count = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Jogador jogador1 = new JurandirOJogador();
+                Jogador jogador2 = Oponente;
+                Jogador jogador3 = new JurandirOJogador();
+                Jogador jogador4 = Oponente;
+
+                Equipe equipe1 = new Equipe(new List<Jogador>() { jogador1, jogador3 });
+                Equipe equipe2 = new Equipe(new List<Jogador>() { jogador2, jogador4 });
+
+                Mesa mesaDeTruco = new Mesa(new List<Equipe>() { equipe1, equipe2 });
+                mesaDeTruco.Jogar();
+
+                if(equipe1.PontosEquipe == 15)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+
+
+
+    }
+}
