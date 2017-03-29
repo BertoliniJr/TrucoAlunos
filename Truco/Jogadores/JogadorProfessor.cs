5b9262dcc73891f71ae3ee8b242e46768f2b03db
Hélio <hbjunior@attps.com.br>
2017-03-29 11:43:42 -0300

    professor temporario

--- Truco/Jogadores/JogadorProfessor.cs
+++ Truco/Jogadores/JogadorProfessor.cs
@@ -13,14 +13,23 @@ namespace CardGame
             nome = $"Professor {n}";
             cartasUsadas = new List<Carta>();
             trucoAtual = null;
+            cartasNaoUasadas = geraBaralho();
         }
 
         private List<Carta> cartasUsadas;
         private Truco? trucoAtual;
+        private List<Carta> cartasNaoUasadas;
+        private int pontosRodada;
 
         public override Carta Jogar(List<Carta> cartasRodada, Carta manilha)
         {
-
+            if (_mao.Count == 3)
+            {
+                foreach (var item in _mao)
+                {
+                    cartasNaoUasadas.Remove(item);
+                }
+            }
         }
 
         public override void NovaMao()
@@ -28,6 +37,35 @@ namespace CardGame
             base.NovaMao();
             cartasUsadas = new List<Carta>();
             trucoAtual = null;
+            cartasNaoUasadas = geraBaralho();
+        }
+
+        private static List<Carta> geraBaralho()
+        {
+            List<Carta> retorno = new List<Carta>();
+            for (int i = 1; i < 14; i++)
+            {
+                if (i != 8 && i != 9 && i != 10)
+                {
+                    retorno.Add(new Carta(Naipes.copas, i));
+                    retorno.Add(new Carta(Naipes.espadas, i));
+                    retorno.Add(new Carta(Naipes.ouros, i));
+                    retorno.Add(new Carta(Naipes.paus, i));
+                }
+            }
+            return retorno;
+        }
+
+        public override void novaCarta(Carta carta, Jogador jogador, Carta manilha)
+        {
+            cartasNaoUasadas.Remove(carta);
+        }
+
+        private int probabilidadeVitoria(Carta manilha)
+        {
+            if (pontosRodada == 1)
+            cartasNaoUasadas.Remove(manilha);
+            double cartasMelhoresDisponivies = cartasNaoUasadas.Where(x => x.valor(manilha) >= (_mao.ma));
         }
     }
 }
