ab4167ee088ae5fdd7f02abb28e9bab3bbc90e11
Farrael <andra@DESKTOP-G2BH1MF>
2017-04-04 14:53:00 -0300

    Fabrica de Baralho

---
+++ Truco/Fábricas/FabricaBaralho.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Truco.Interfaces;
+using Truco.Enumeradores;
+using Truco.Baralhos;
+
+namespace Truco.Fábricas
+{
+    class FabricaBaralho
+    {
+        public static IBaralho CriarBaralho(IJogo jogo)
+        {
+            if(jogo.tipoJogo == EnumTipoJogo.truco)
+            {
+                IBaralho baralho = new BaralhoTruco();
+                return baralho;
+            }
+            return null;            
+        }
+    }
+}
