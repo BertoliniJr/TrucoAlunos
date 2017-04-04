1dd3038fd98fd66e21caa6b329ed1ab5ad6613fd
Dupla IV (Victoria and Patrick) <aluno@BRPLI0428>
2017-04-04 14:27:51 -0300

    Criação da Pasta Fábrica

---
+++ Truco/Fábricas/FabricaRodada.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Truco.Interfaces;
+using Truco.Enumeradores;
+
+namespace Truco.Fábricas
+{
+    public static class FabricaRodada
+    {
+        public static IRodada CriarRodada()
+        {
+            EnumTipoJogo jogo = (EnumTipoJogo)Jogo.getJogo().infoJogo;
+            switch (jogo)
+            {
+                case EnumTipoJogo.truco:
+                    throw new NotImplementedException();
+                  
+                case EnumTipoJogo.poker:
+                    throw new NotImplementedException();
+                case EnumTipoJogo.malmal:
+                    throw new NotImplementedException();
+                case EnumTipoJogo.buraco:
+                    throw new NotImplementedException();
+                case EnumTipoJogo.pife:
+                    throw new NotImplementedException();
+                default:
+                    break;
+            }
+            return null;
+        }
+
+
+    }
+}
